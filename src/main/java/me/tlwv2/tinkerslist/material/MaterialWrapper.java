8681d51ead8465e5c68fee78bba686dbcb094864
Plasmatic <unconfigured@null.spigotmc.org>
2018-07-02 23:34:25 -0400

    more backending???

--- src/main/java/me/tlwv2/tinkerslist/material/MaterialWrapper.java
+++ src/main/java/me/tlwv2/tinkerslist/material/MaterialWrapper.java
@@ -2,23 +2,111 @@ package me.tlwv2.tinkerslist.material;
 
 import slimeknights.tconstruct.library.materials.Material;
 
-public class MaterialWrapper implements Comparable<MaterialWrapper>{
-    //TODO: stuff
+import java.util.HashMap;
+
+public class MaterialWrapper implements Comparable<MaterialWrapper>, Cloneable{
     private Material material;
-    private SortingValue sortingValue;
+    private HashMap<String, Boolean> registeredCategories;
+    private HashMap<String, HashMap<String, StatValue>> stats;
+
+    private String currentCategory;
+    private String currentStat;
 
     public MaterialWrapper(Material material){
         this.material = material;
-        this.sortingValue = null;
+        this.currentCategory = null;
+        this.currentStat = null;
+        this.registeredCategories = new HashMap<>();
+        this.stats = new HashMap<>();
+
+        for(String category : Util.MATERIAL_TYPES){
+            registeredCategories.put(category, false);
+        }
     }
+
+    public void registerCategory(String category){
+        registeredCategories.put(category, true);
+    }
+
+    public void setCategory(String category){
+        currentCategory = category;
+    }
+
+    public void setStat(String stat){
+        currentStat = stat;
+    }
+
+    public void registerStat(String stat, Comparable value){
+        stats.get(currentCategory).put(stat, new StatValue(value));
+    }
+
+    public void nullifyCurrents(){
+        currentCategory = null;
+        currentStat = null;
+    }
+
+    public StatValue getCurrentStat(){
+        return stats.get(currentCategory).get(currentStat);
+    }
+
+    public String getType(){
+        return getCurrentStat().getClass().getSimpleName();
+    }
+
+    public MaterialWrapper reproduceFinalVersion(){
+        final MaterialWrapper wrapper = new MaterialWrapper(material);
+        wrapper.registerCategory(currentCategory);
+        wrapper.setCategory(currentCategory);
+        wrapper.registerStat(currentStat, new StatValue(getCurrentStat()));
+        return wrapper;
+    }
+
 
     @Override
     public int compareTo(MaterialWrapper o) {
-        return 0; //TODO: compareTo of MaterialWrapper
+        return stats.get(currentCategory).get(currentStat).compareTo(o);
     }
 
     @Override
     public String toString() {
         return ""; //TODO: toString of MaterialWrapper
+    }
+
+    //Getter+Setter
+
+    public Material getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(Material material) {
+        this.material = material;
+    }
+
+    public HashMap<String, Boolean> getRegisteredCategories() {
+        return registeredCategories;
+    }
+
+    public void setRegisteredCategories(HashMap<String, Boolean> registeredCategories) {
+        this.registeredCategories = registeredCategories;
+    }
+
+    public HashMap<String, HashMap<String, StatValue>> getStats() {
+        return stats;
+    }
+
+    public void setStats(HashMap<String, HashMap<String, StatValue>> stats) {
+        this.stats = stats;
+    }
+
+    public String getCurrentCategory() {
+        return currentCategory;
+    }
+
+    public void setCurrentCategory(String currentCategory) {
+        this.currentCategory = currentCategory;
+    }
+
+    public void setCurrentStat(String currentStat) {
+        this.currentStat = currentStat;
     }
 }
--- src/main/java/me/tlwv2/tinkerslist/material/MaterialWrapperManager.java
+++ src/main/java/me/tlwv2/tinkerslist/material/MaterialWrapperManager.java
@@ -1,12 +1,74 @@
 package me.tlwv2.tinkerslist.material;
 
+import slimeknights.tconstruct.library.TinkerRegistry;
+import slimeknights.tconstruct.library.materials.IMaterialStats;
+import slimeknights.tconstruct.library.materials.Material;
+
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MaterialWrapperManager {
-    //TODO: Actually add code here
-    private ArrayList<MaterialWrapper> materials;
+    private static ArrayList<MaterialWrapper> materials;
 
     public static void init(){
-        //
+        for(Material material : TinkerRegistry.getAllMaterials()){
+            MaterialWrapper wrapper = new MaterialWrapper(material);
+
+            for(String category : Util.MATERIAL_TYPES){
+                if(material.hasStats(category)){
+                    IMaterialStats stats = material.getStats(category);
+
+                    wrapper.registerCategory(category);
+                    wrapper.setCategory(category);
+
+                    for(String statName : Util.statsForCategory(category)){
+                        Field field = null;
+
+                        try {
+                            field = stats.getClass().getField(statName);
+                        } catch (NoSuchFieldException e) {
+                            e.printStackTrace();
+                        }
+
+                        try {
+                            wrapper.registerStat(field.getName(), (Comparable) field.get(material));
+                        } catch (IllegalAccessException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+
+            wrapper.nullifyCurrents();
+            materials.add(wrapper);
+        }
+    }
+
+    //Dyn
+
+    private ArrayList<MaterialWrapper> currentMaterials;
+    private HashMap<String, Boolean> applied;
+
+    public MaterialWrapperManager(){
+        this.currentMaterials = new ArrayList<>();
+        this.applied = new HashMap<>();
+
+        for(MaterialWrapper materialWrapper : materials){
+            applied.put(materialWrapper.getMaterial().getIdentifier(), true);
+        }
+    }
+
+    public void applyCategory(String category){
+        //TODO
+    }
+
+    public void applyFilter(String filterString){
+        //TODO
+    }
+
+    public ArrayList<String> getMaterialStrings(){
+        //TODO: tostring
+        return null;
     }
 }
